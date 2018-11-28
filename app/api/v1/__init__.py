6015d31cb0d78543abc542424bd527cc370629af
ObedZac <zacchaeus.obed@gmail.com>
2018-11-28 11:22:49 +0300

    [starts #162263044] set up the blue prints

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask-restplus import Api
+from views.redflags import RedFlags, RedFlag
+
+version_one = Blueprint("v1", __name__, url_prefix="/api/v1")
+API = Api(version_one)
+
+
+API.add_resource(RedFlags, '/redflags/')
+API.add_resource(RedFlag, '/redflags/<int:id>'/)
