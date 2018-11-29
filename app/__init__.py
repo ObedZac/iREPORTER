8f69196a3a8d1f21403924b3e48fdf24a63f096d
ObedZac <zacchaeus.obed@gmail.com>
2018-11-29 12:06:57 +0300

    [finishes #162300268] Setup app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask, Blueprint
-
 from .api.v1 import version_one as v1
+from instance.config import app_config
+# from .db_config import create_tables
 
 
-def create_app():
-    APP= Flask(__name__)
+def create_app(config_name='development'):
+    APP = Flask(__name__, instance_relative_config = True)
+    APP.url_map.strict_slashes = False
+    APP.config.from_object(app_config[config_name])
+    APP.config.from_pyfile('config.py')
+
+    # create-tables()
+
     APP.register_blueprint(v1)
+
     return APP
+
+
+APP = create_app()
+
+
+# def create_app():
+#     APP= Flask(__name__)
+#     APP.register_blueprint(v1)
+#     return APP
